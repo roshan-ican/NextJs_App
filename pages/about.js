659c879b7815e9e0cda76f7d9799c2ee7bbfb38c
Muhammad Roshan ali <66337567+roshan-ican@users.noreply.github.com>
2022-07-23 10:10:38 +0530

    first push

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        About my Fake Store, it is a store where you can get anything but it
+        will be nothing just a delusion
+      </p>
+      <p>Version: 2.0.0</p>
+    </div>
+  );
+}
